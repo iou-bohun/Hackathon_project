35af2b643093a2d348515b0821617c717818b9c3
hjgf0624 <hjgf0624@gmail.com>
2022-11-18 22:04:45 +0900

    4rd

---
+++ app/src/main/java/kr/ac/jbnu/se/hackathon_project/LoginActivity.java
@@ -0,0 +1,95 @@
+package kr.ac.jbnu.se.hackathon_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.internal.service.Common;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edt_SignInStudentNumber;
+    EditText edt_SignInPassword;
+    Button btn_login;
+    Button btn_join;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        edt_SignInStudentNumber = (EditText) findViewById(R.id.studentId);
+        edt_SignInPassword = (EditText) findViewById(R.id.password);
+        btn_login = (Button) findViewById(R.id.login);
+        btn_join = (Button) findViewById(R.id.join);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(LoginActivity.this);
+                mDialog.setMessage("기다려 주세요.");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.child(edt_SignInStudentNumber.getText().toString()).exists())
+                        {
+                            mDialog.dismiss();
+                            UserInfo user = snapshot.child(edt_SignInStudentNumber.getText().toString()).getValue(UserInfo.class);
+
+                            if(user.getPassword().equals(edt_SignInPassword.getText().toString()))
+                            {
+                                Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                //?
+                                startActivity(homeIntent);
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(LoginActivity.this,"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "계정이 없거나 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent SignUpIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(SignUpIntent);
+                finish();
+            }
+        });
+    }
+
+
+}
